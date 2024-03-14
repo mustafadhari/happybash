d7e52d55962b95a5bd809fc0be9f4d22cac333aa
mustafdhari <mdhariwala033@gmail.com>
2024-03-15 00:59:12 +0530

    cart item model update

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class CartItem extends Model
 {
     use HasFactory;
-    protected $fillable = ['cart_id', 'product_id', 'quantity'];
+    protected $fillable = ['cart_id', 'product_id', 'quantity', 'start_date', 'end_date'];
 
     public function cart()
     {
