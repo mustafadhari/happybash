f21649a74b88cf8da3f916498302764da7d8083b
mustafdhari <mdhariwala033@gmail.com>
2024-04-08 14:47:26 +0530

    .

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Twilio\Rest\Client;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 
