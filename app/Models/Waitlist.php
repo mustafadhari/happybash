254d376ea29eb0945b49c18ca06a2eddf9c33fff
mustafdhari <mdhariwala033@gmail.com>
2024-05-23 12:05:41 +0530

    update waitlist model

--- app/Models/Waitlist.php
+++ app/Models/Waitlist.php
@@ -9,4 +9,8 @@ class Waitlist extends Model
 {
     use HasFactory;
     protected $fillable = ['user_id', 'product_id', 'start_date', 'end_date'];
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
